83f70d7e21b7ec8aa0e788db7f00ba0eabc109bd
Nell Hardcastle <chizu@spicious.com>
2010-12-03 16:48:16 -0800

    Working simplex noise.

--- noise.c
+++ noise.c
@@ -1,32 +1,95 @@
 #include <Python.h>
-#include "noise.h"
+#include "include/noise.h"
 
-float dot2(float2 m, float2 n) {
-	return m.x*n.x + m.y*n.y;
+float dot2(const double * m, double2 n) {
+	return m[0]*n.x + m[1]*n.y;
 }
 
-float dot3(float3 m, float3 n) {
-	return m.x*n.x + m.y*n.y + m.z*n.z;
+float dot3(const double * m, double3 n) {
+	return m[0]*n.x + m[1]*n.y + m[2]*n.z;
 }
 
-float dot4(float4 m, float4 n) {
-	return m.x*n.x + m.y*n.y + m.z*n.z + m.w*n.w;
+float dot4(const double * m, double4 n) {
+	return m[0]*n.x + m[1]*n.y + m[2]*n.z + m[3]*n.w;
 }
 
 static PyObject *
-snoise(PyObject *self, PyObject *args) {
-	int x, y, octaves;
-	float noise;
-	if (!PyArg_ParseTuple(args, "i|i|i", &x, &y, &octaves))
+snoise2d(PyObject *self, PyObject *args) {
+	double x, y;
+	if (!PyArg_ParseTuple(args, "d|d", &x, &y))
 		return NULL;
 
-	// Stub for now
-	
-	return Py_BuildValue("i", 0);
+	// Noise contributions from the three corners
+	double n0, n1, n2;
+
+	// Skew the input space to determine which simplex cell we're in
+	double F2 = 0.5*(sqrt(3.0) - 1.0);
+	double s = (x + y) * F2;
+	int i = floor(x+s);
+	int j = floor(y+s);
+
+	double G2 = (3.0 - sqrt(3.0))/6.0;
+	double t = (i+j) * G2;
+	double X0 = i-t; // Unskew the cell origin back to (x,y) space
+	double Y0 = j-t;
+	// The x,y distances from the cell origin
+	double2 zero = {{x-X0, y-Y0}};
+
+	// For the 2D case, the simplex shape is an equilateral triangle
+	// Determine which simplex we are in.
+	int i1, j1;
+	if (zero.x>zero.y) {
+		i1=1; j1=0;
+	}
+	else {
+		i1=0; j1=1;
+	}
+
+	double2 one = {{zero.x - i1 + G2, 
+					zero.y - j1 + G2}};
+	double2 two = {{zero.x - 1.0 + 2.0 * G2,	
+					zero.y - 1.0 + 2.0 * G2}};
+
+	// Work out the hashed gradient indices of the three simplex corners
+	int ii = i & 255;
+	int jj = j & 255;
+	int gi0 = p[ii + p[jj]] % 12;
+	int gi1 = p[ii + i1 + p[jj] + j1] % 12;
+	int gi2 = p[ii + i1 + p[jj] + j1] % 12;
+
+	// Calculate the contribution from the three corners
+	double t0 = 0.5 - zero.x*zero.x-zero.y*zero.y;
+	if (t0 < 0) {
+		n0 = 0.0;
+	}
+	else {
+		t0 *= t0;
+		n0 = t0 * t0 * dot2(GRAD3[gi0], zero);
+	}
+
+	double t1 = 0.5 - one.x*one.x-one.y*one.y;
+	if(t1<0) {
+		n1 = 0.0;
+	}
+	else {
+		t1 *= t1;
+		n1 = t1 * t1 * dot2(GRAD3[gi1], one);
+	}
+
+	double t2 = 0.5 - two.x*two.x-two.y*two.y;
+	if(t2<0) {
+		n2 = 0.0;
+	}
+	else {
+		t2 *= t2;
+		n2 = t2 * t2 * dot2(GRAD3[gi2], two);
+	}
+
+	return Py_BuildValue("d", 70 * (n0 + n1 + n2));
 }
 
 PyMethodDef methods[] = {
-    {"snoise", snoise, METH_VARARGS, "Calculate simplex noise for a coordinate."},
+    {"snoise2d", snoise2d, METH_VARARGS, "Calculate simplex noise for a coordinate."},
     {NULL, NULL, 0, NULL}
 };
 
--- setup.py
+++ setup.py
@@ -2,4 +2,6 @@ from distutils.core import setup, Extension
 
 setup(name = "Noise",
       version = "1.0",
-      ext_modules = [Extension("noise", ["noise.c"])])
+      ext_modules = [Extension("noise", ["noise.c"], include_dirs=['include'])])
+
+
